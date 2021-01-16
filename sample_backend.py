c65c33f022a945a240715faecc6cc43ad331329f
Baagi <secondo@Bats-macbook.local>
2021-01-15 21:34:00 -0800

    Added DELETE method to delete users.

--- sample_backend.py
+++ sample_backend.py
@@ -56,12 +56,20 @@ def get_users():
       # 200 is the default code for a normal response
       return resp
 
-@app.route('/users/<id>')
-def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
-
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def get_user(id):   
+    if id :
+        if request.method == 'GET':
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        elif request.method == 'DELETE':
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].remove(user)
+                    resp = jsonify(), 204
+                    return resp
+            resp = jsonify({"msg:": "user id not found"}), 404
+            return resp
+    return users
